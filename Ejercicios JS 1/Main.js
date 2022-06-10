af7b87be9ddfcc3092a4884ec49f128bdf963510
Eze1995IRESM <74332400+Eze1995IRESM@users.noreply.github.com>
2022-06-10 16:40:31 -0300

    Add files via upload

---
+++ Ejercicios JS 1/Main.js
@@ -0,0 +1,77 @@
+/*Correspondiente a ejercicio1*/
+
+/*let nombre= 'Ezequiel ';
+let edad= 27;
+document.write(nombre);
+document.write('<br>')
+document.write(edad);
+console.log(nombre)
+console.log(edad)*/
+
+/*Correspondiente a ejercicio 2*/
+/*let nombre='';
+let sueldo='';
+nombre='Diez Nicolas';
+sueldo='75000';
+document.write(nombre +'<br>'+ sueldo)*/
+
+/*Correspondiente a ejercicio 3*/
+/*let usuario;
+let mail;
+usuario=prompt('Ingrese el nombre de usuario')
+mail=prompt('Ingrese una direccion de correo electronico')
+document.write('Nombre de usuario:    ')
+document.write(usuario)
+document.write('<br>')
+document.write('Direccion de correo electronico:    ')
+document.write(mail)*/
+
+/*Correspondiente a ejercicio 4*/
+/*let lado;
+let perimetro;
+lado=prompt('Ingrese el largo del lado en cm')
+perimetro=(lado*4)
+document.write('El perimetro del cuadrado es de: '+perimetro+' centimetros')*/
+
+/*Correspondiente a ejercicio 5*/
+/*let primerNumero;
+let segundoNumero;
+let tercerNumero;
+let cuartoNumero;
+let suma;
+let producto;
+primerNumero=parseInt(prompt('Ingrese el primer numero'))
+segundoNumero=parseInt(prompt('Ingrese el segundo numero'))
+suma=((primerNumero+segundoNumero))
+tercerNumero=prompt('Ingrese el tercer numero')
+cuartoNumero=prompt('Ingrese el cuarto numero')
+producto=(tercerNumero*cuartoNumero)
+document.write('El resultado de la suma entre el primer y segundo numeros es: '+suma)
+document.write('<br>')
+document.write('El producto entre el tercer y cuarto producto es: '+producto)*/
+
+/* Correspondiente a ejercicio 6*/
+/*let primerNumero;
+let segundoNumero;
+let tercerNumero;
+let cuartoNumero;
+let suma;
+let producto;
+primerNumero=parseInt(prompt('Ingrese el primer numero'))
+segundoNumero=parseInt(prompt('Ingrese el segundo numero'))
+tercerNumero=parseInt(prompt('Ingrese el tercer numero'))
+cuartoNumero=parseInt(prompt('Ingrese el cuarto numero'))
+suma=((primerNumero+segundoNumero+tercerNumero+cuartoNumero))
+producto=(primerNumero*segundoNumero*tercerNumero*cuartoNumero)
+document.write('El resultado de la suma entre todos los numeros es: '+suma)
+document.write('<br>')
+document.write('El producto entre todos los numeros es: '+producto)*/
+
+/*Correspondiente a ejercicio 7*/
+/*let precio;
+let cantidad;
+let total;
+precio=parseInt(prompt('Ingrese el precio del producto'))
+cantidad=parseInt(prompt('Ingrese la cantidad de unidades'))
+total=(precio*cantidad)
+document.write('El total a abonar es de: '+total+' UM')*/
